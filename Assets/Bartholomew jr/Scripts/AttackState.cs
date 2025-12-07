02220b275802d1a07e208d64d9f9ac37691482bc
abeat <abeat@LAPTOP-63SGOAI8>
2025-12-07 14:14:46 +0000

    updated Attack state

---
+++ Assets/Bartholomew jr/Scripts/AttackState.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackState : BaseState
+{
+    private SmartTank tank;
+
+    public AttackState(SmartTank tank)
+    {
+        this.tank = tank;
+    }
+
+    public override Type StateEnter()
+    {
+        return null;
+    }
+
+    public override Type StateExit()
+    {
+        return null;
+    }
+
+    public override Type StateUpdate()
+    {
+        time += Time.deltaTime;
+        if (time > 1f)
+        {
+            if (tank.stats["lowHealth"] == true)
+            {
+                return typeof(FleeState);
+            }
+            if (tank.stats["targetReached"] == true)
+            {
+                return typeof(AttackState);
+            }
+            else
+            {
+                if (tank.stats["enemyVisible"] == true)
+                {
+                    return typeof(ChaseState);
+                }
+
+                return typeof(PatrolState);
+            }
+             
+        }
+        return null;
+    }
+}
